c81d18827501c7087ec0674826e9374dcb650258
Ben McKinzie Jr <33335386+benmckinzie@users.noreply.github.com>
2020-07-28 13:56:05 -0500

    Update InstallSysmonWorkstation.bat

--- InstallSysmonWorkstation.bat
+++ InstallSysmonWorkstation.bat
@@ -4,7 +4,7 @@ pushd "C:\ProgramData\sysmon\"
 echo [+] Downloading Sysmon...
 @powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\ProgramData\sysmon\sysmon64.exe')"
 echo [+] Downloading Sysmon config...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/benmckinzie/sysmontest/master/workstation_sysmonconfig.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/benmckinzie/sysmon/master/workstation_sysmonconfig.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
 sysmon64.exe -accepteula -i sysmonconfig-export.xml
 sc failure Sysmon64 actions= restart/10000/restart/10000// reset= 120
 echo [+] Sysmon Successfully Installed!
